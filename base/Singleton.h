4ed2e49cb3166cc413a6cea070d45e09a246b985
zhangheihei <18700939@qq.com>
2016-04-12 00:56:52 +0800

    update

---
+++ base/Singleton.h
@@ -0,0 +1,61 @@
+#include "noncopyable.h"
+#include <assert.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+namespace muduo
+{
+namespace detail
+{
+template<typename T>
+struct has_no_destroy
+{
+	template <typename c> static char test(decltype(&C::no_destroy));
+	template <typename C> static int32_t test(...);
+	const static bool value = sizeof(test<T>(0)) == 1;
+};
+}//deatail
+
+template<typename T>
+class Singleton : noncopyable
+{
+public:
+	static T& instance();
+	{
+		pthread_once(&ponce_, &Singleton::init);
+		assert(value_ != NULL);
+		return *value_;
+	}
+private:
+	Singleton();
+	~Singleton();
+	
+	static void init()
+	{
+		value_ = new T();
+		if (!detail::has_no_destroy<T>::value)
+		{
+			::atexit(destroy);
+		}
+	}
+
+	static void destroy()
+	{
+		typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+		T_must_be_complete_type dummy; (void) dummy;
+
+		delete value_;
+		value_ = NULL;
+	}
+
+private:
+	static pthread_once_t ponce_;
+	static T* value_;
+};//Singleton
+}//muduo
+
+template<typename T>
+pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;
+
+template<typename T>
+T* Singleton<T>::value_ = NULL;
